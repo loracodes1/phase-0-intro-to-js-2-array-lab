2fec59433611bf45b762cc0519b940bc2f2e05fe
florence <loracodes3@gmail.com>
2024-10-14 20:12:36 +0300

    buiy

--- index.js
+++ index.js
@@ -15,6 +15,15 @@ function destructivelyAppendCat(name) {
 function destructivelyRemoveFirstCat(){
     cats.shift()
 }
-function appendcat(name){
+function appendCat(name){
     return [...cats,name]
+}
+function prependCat(name){
+    return [name, ...cats]
+}
+function removeLastCat(){
+    return cats.slice(0, cats.length - 1)
+}
+function removeFirstCat(){
+    return cats.slice(1, cats.length)
 }
